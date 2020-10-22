f9a4bf5e5a12f9b9d8288bcc815f6d0de14bcced
octitesta <octis.testa@gmail.com>
2020-10-21 21:10:57 -0300

    Admin creado y def __str__

--- Inmobibliaria/admin.py
+++ Inmobibliaria/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from.models import *
 
 # Register your models here.
+
+class PropiedadAdmin(admin.ModelAdmin):
+    list_display = ['usuario','titulo',]
+
+class ConsultaAdmin(admin.ModelAdmin):
+    list_display = ['usuario','propiedad','mensaje']
+
+admin.site.register(Contacto)
+admin.site.register(Usuario)
+admin.site.register(Propiedad,PropiedadAdmin)
+admin.site.register(Consulta,ConsultaAdmin)
--- Inmobibliaria/models.py
+++ Inmobibliaria/models.py
@@ -9,18 +9,25 @@ class Usuario(models.Model):
     nombre = models.CharField(max_length=20)
     contraseña = models.CharField(max_length=20)
     contacto = models.ForeignKey('Contacto', on_delete=models.CASCADE, null=False)
+    def __str__(self):
+        return "{}".format(self.nombre)
+
 
 class Propiedad(models.Model):
+    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE, null=False)
     titulo = models.CharField(max_length=20)
     ubicacion = models.CharField(max_length=20)
     habitaciones = models.IntegerField(max_length=20)
     dormitorios = models.IntegerField(max_length=20)
     baños = models.IntegerField(max_length=20)
     imagenes = models.ImageField(max_length=100, upload_to='fotos/', blank=True)
-    descripcion = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=200)
+    def __str__(self):
+        return "{}".format(self.titulo)
+
 
 class Consulta(models.Model):
     usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE, null=False)
     propiedad = models.ForeignKey('Propiedad', on_delete=models.CASCADE, null=False)
-    mensaje = models.CharField(max_length=20)
- 
+    mensaje = models.CharField(max_length=200)
+
